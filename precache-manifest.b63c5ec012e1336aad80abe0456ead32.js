99dde73484070c5476b69ffe225e3f64b8e48659
Moataz Farid <Moataz.Farid@Qcentriseg.com>
2019-12-09 16:29:54 +0200

    Updates

---
+++ precache-manifest.b63c5ec012e1336aad80abe0456ead32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779dda1b7cadba2e32c86b2044006b9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc5b6e3acdd441f60f8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d772e10cb521039c4918",
+    "url": "/static/js/2.e99c63d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e99c63d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dc5b6e3acdd441f60f8",
+    "url": "/static/js/main.160d118d.chunk.js"
+  },
+  {
+    "revision": "9c44bcdd2dc8c84194ff",
+    "url": "/static/js/runtime-main.149cc60b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "MoatazFarid.github.io/moataz-farid-website/precache-manifest.fabffe911d5530957dd4f984fce77248.js"
+  "/precache-manifest.b63c5ec012e1336aad80abe0456ead32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("MoatazFarid.github.io/moataz-farid-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
